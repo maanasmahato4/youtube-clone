13f51d8c86b09ebbe1d5a66ee785b09208d94f70
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-14 17:44:47 +0545

    [feat]: sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,3 +1,10 @@
+import styles from './sidebar.module.css';
+
 export default function SideBar() {
-	return <div>sidebar</div>;
+	return (
+		<div className={styles['sidebar-container']}>
+			<div className={styles['card']}></div>
+			<div className={styles['card']}></div>
+		</div>
+	);
 }
