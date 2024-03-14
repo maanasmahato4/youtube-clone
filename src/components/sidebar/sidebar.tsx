49db13d9eb3c2e5545976e054627706749510af5
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-14 20:55:48 +0545

    [feat]: sidebar video list display

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,10 +1,26 @@
+import React from 'react';
 import styles from './sidebar.module.css';
+import { VideoDetails } from '../../pages/home/home';
 
-export default function SideBar() {
+type sideBarProps = {
+	videos: VideoDetails[];
+};
+
+const SideBar: React.FC<sideBarProps> = ({ videos }): React.ReactElement => {
+	console.log(videos);
 	return (
 		<div className={styles['sidebar-container']}>
-			<div className={styles['card']}></div>
-			<div className={styles['card']}></div>
+			{videos.map((video, idx) => {
+				if (video !== undefined) {
+					return (
+						<div key={idx} className={styles['card']}>
+							<img height={'100%'} width={'100%'} src={video.thumbNail} />
+						</div>
+					);
+				}
+			})}
 		</div>
 	);
-}
+};
+
+export default SideBar;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,12 +5,36 @@ import VideoScreen from '../../components/video-screen/video-screen';
 import styles from './home.module.css';
 import { HttpRequest } from '../../lib/httpRequest/httpRequest';
 
+export type VideoDetails = {
+	title: string;
+	videoId: string;
+	views: string;
+	thumbNail: string;
+};
+
 function Home() {
 	const [searchValue, setSearchValue] = React.useState<string>('');
 	const [videoUrl, setVideoUrl] = React.useState<string>('');
+	const [videoList, setVideoList] = React.useState<VideoDetails[]>([]);
 
 	async function handleSearch(): Promise<void> {
 		const response = await HttpRequest.searchVideos(searchValue);
+		const videosDetails = response.data.contents.map(
+			(content: any): VideoDetails | undefined => {
+				if (content.type === 'video') {
+					const { title, videoId, stats, thumbnails } = content.video;
+					if (thumbnails[0] || thumbnails[1]) {
+						return {
+							title,
+							videoId,
+							views: stats.views,
+							thumbNail: thumbnails[0].url || thumbnails[1].url,
+						};
+					}
+				}
+			},
+		);
+		setVideoList(videosDetails);
 		const url = `https://youtube.com/embed/${response.data.contents[0].video.videoId}`;
 		setVideoUrl(url);
 	}
@@ -27,7 +51,7 @@ function Home() {
 				<VideoScreen videoUrl={videoUrl} />
 			</section>
 			<section>
-				<SideBar />
+				<SideBar videos={videoList} />
 			</section>
 		</main>
 	);
